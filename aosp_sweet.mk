c6fc1604710fdf60089886a1a5d06f2c988b511e
aarav <107787044+kangeraarav@users.noreply.github.com>
2022-07-13 21:26:38 +0530

    Update aosp_sweet.mk

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -11,14 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PEX stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Project Arcana stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# PEX Stuff
+# maintainer flag
+ARCANA_MAINTAINER := aarav and shubham
+WITH_GAPPS := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
 TARGET_SUPPORTS_QUICK_TAP := true
-
+USE_PIXEL_CHARGER_IMAGES := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_sweet
 
